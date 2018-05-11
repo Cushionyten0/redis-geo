9c96133f67bae9cd5c20ea9e4a469309edf65025
Aleksandr <cushionyten0@gmail.com>
2018-05-10 23:54:43 -0400

    UPDATED TO USE ELASTIC IP

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -6,7 +6,7 @@ export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-2
 export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=ec2-18-191-69-21.us-east-2.compute.amazonaws.com
+export AWS_VIRTUAL_HOST=18.188.190.238
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
